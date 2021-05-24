2c19af3d8f3cd83e18347f767a1594f28c08a0ef
David Mwendwa <klatzdavie19@gmail.com>
2021-05-24 12:41:11 +0300

    Create Burger component It will hold BurgerIngredient component that will be used to build the burger

--- src/containers/BurgeBuilder/BurgerBuilder.js
+++ src/containers/BurgeBuilder/BurgerBuilder.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import Burger from '../../components/Burger/Burger'
+
 import Auxiliary from '../../hoc/Auxiliary';
+
 
 class BurgerBuilder extends React.Component {
   render() {
     return (
       <Auxiliary>
-        <div>Burger</div>
+        <Burger />
         <div>Build Controls</div>
       </Auxiliary>
     );
