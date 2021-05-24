ff893be1e13b952dfb339a738e33b9b888b5f2f3
David Mwendwa <klatzdavie19@gmail.com>
2021-05-24 12:28:31 +0300

    Create BurgerBuilder container component Will contain Burger and BuildControls for the burger

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+
 class App extends Component {
   render() {
     return (
       <div>
-        Burger Buider
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
---
+++ src/containers/BurgeBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Auxiliary from '../../hoc/Auxiliary';
+
+class BurgerBuilder extends React.Component {
+  render() {
+    return (
+      <Auxiliary>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </Auxiliary>
+    );
+  }
+}
+
+export default BurgerBuilder;
