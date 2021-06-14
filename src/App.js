7cc05b1f95547b4f0eb66009578c02a6106d1704
David Mwendwa <klatzdavie19@gmail.com>
2021-06-14 16:52:55 +0300

    refactor: change app folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
-import Layout from './components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 
-import {BurgerBuilder} from './containers/BurgeBuilder/BurgerBuilder'
+import { BurgerBuilder } from './containers/BurgeBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
@@ -16,4 +16,4 @@ class App extends Component {
   }
 }
 
-export default App;  
+export default App;
