5f210c41e695eddf1d464a2a707ed5f4d5a1831e
David Mwendwa <klatzdavie19@gmail.com>
2021-06-14 10:21:14 +0300

    feat: add a toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 import classes from './Layout.css';
 
 const Layout = ({ children }) => (
   <Auxiliary>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>{children}</main>
   </Auxiliary>
 );
