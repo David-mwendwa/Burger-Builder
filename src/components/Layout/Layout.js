5ba752855109019a075c7660c7a2e1f4a0e3a405
David Mwendwa <klatzdavie19@gmail.com>
2021-06-14 11:14:24 +0300

    feat: create a responsive side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.css';
 
 const Layout = ({ children }) => (
   <Auxiliary>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{children}</main>
   </Auxiliary>
 );
