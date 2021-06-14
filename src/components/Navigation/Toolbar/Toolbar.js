816bceda5f93b48bb4e790aa60584b0bb70a915d
David Mwendwa <klatzdavie19@gmail.com>
2021-06-14 14:53:58 +0300

    feat: add side drawer toggle icon with functionality

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 import classes from './Toolbar.css';
 
 const Toolbar = (props) => (
   <header className={classes.Toolbar} style={{ height: props.height }}>
-    <div>MENU</div>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
     <div className={classes.Logo}>
       <Logo />
     </div>
