085ae09a96560cf27b7f1a82076106b2e974435d
David Mwendwa <klatzdavie19@gmail.com>
2021-06-14 09:41:55 +0300

    feat: add a custom button component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,6 +18,8 @@ const OrderSummary = ({ ingredients }) => {
       <p>A delicious burger with the following ingredients</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to checkout?</p>
+      <button>CANCEL</button>
+      <button>CONTINUE</button>
     </Auxiliary>
   );
 };
