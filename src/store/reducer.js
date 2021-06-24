4e51f843bbeec66eb1e11e99c2fbb535959b096a
David Mwendwa <klatzdavie19@gmail.com>
2021-06-24 09:53:14 +0300

    feat: basic redux setup

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from './actions'
+
+const INITIAL_STATE = {
+  ingredients: null,
+  totalPrice: 4
+}
+
+const reducer = (state = INITIAL_STATE, action) => {
+
+}
+
+export default reducer
